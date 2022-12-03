90002afb0eea4e1126c9292d08ad954126d3a594
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-02 19:21:46 -0800

    Installs axios lib and creates ENV for baseURL in App.jsx for localhost:3000 at backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import axios from "axios";
 import { Home } from "./Home";
 
 function App() {
+  axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
   return (
     <div>
       <Home />
