a2c53c515d14361261cb7d68ae7e92e7c8463481
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-02 19:43:09 -0800

    Adds axios request to backend for companies indes, useState and useEffect for request and setting companies, and maps over array in return to display company name, url, and region

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,26 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export function Home() {
+  const [companies, setCompanies] = useState([]);
+
+  const handleIndexCompanies = () => {
+    axios.get("/companies.json").then((response) => {
+      setCompanies(response.data);
+      console.log(response.data);
+    });
+  };
+
+  useEffect(handleIndexCompanies, []);
+
   return (
     <div>
       <h1>Remote-Friendly Companies</h1>
+      {companies.map((company) => (
+        <p>
+          {company.name} | {company.url} | {company.region}
+        </p>
+      ))}
     </div>
   );
 }
