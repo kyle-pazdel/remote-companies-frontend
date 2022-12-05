dd357ff6f42470421ac291273334a634e23c2e19
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-05 12:26:42 -0800

    Adds function to handle current company for special highlight styling in Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,30 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 
 export function Home() {
   const [companies, setCompanies] = useState([]);
+  const [currentCompany, setCurrentCompany] = useState({});
   const [searchFilter, setSearchFilter] = useState("");
+  const [perPage, setPerPage] = useState(10);
+  const [page, setPage] = useState(0);
+  const [pages, setPages] = useState(0);
 
   const handleIndexCompanies = () => {
     axios.get("/companies.json").then((response) => {
       setCompanies(response.data);
+      setPages(Math.floor(response.data.length / perPage));
     });
+  };
+
+  const handlePageClick = (event) => {
+    let page = event.selected;
+    setPage(page);
+  };
+
+  const handleCompanyHighlight = (company) => {
+    setCurrentCompany(company);
+    console.log(currentCompany);
   };
 
   useEffect(handleIndexCompanies, []);
@@ -32,7 +48,7 @@ export function Home() {
         {companies
           .filter((company) => company.region?.toLowerCase().includes(searchFilter.toLowerCase()))
           .map((company) => (
-            <div key={company.id} className="row m-2">
+            <div key={company.id} className="row m-2" onClick={() => handleCompanyHighlight(company)}>
               <p className="col">{company.name}</p>{" "}
               <a className="col btn btn-primary" href={company.url} target="_blank">
                 See Site
