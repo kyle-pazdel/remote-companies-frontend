e14866262f5feffed30080ce1877ab7f17792125
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-12-05 12:35:28 -0800

    Adds custom styling for currentCompany and conditional rendering based on selected company in Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -48,7 +48,11 @@ export function Home() {
         {companies
           .filter((company) => company.region?.toLowerCase().includes(searchFilter.toLowerCase()))
           .map((company) => (
-            <div key={company.id} className="row m-2" onClick={() => handleCompanyHighlight(company)}>
+            <div
+              key={company.id}
+              className={company.id === currentCompany.id ? "row pt-2 pb-2 current-company" : "row pt-2 pb-2 company"}
+              onClick={() => handleCompanyHighlight(company)}
+            >
               <p className="col">{company.name}</p>{" "}
               <a className="col btn btn-primary" href={company.url} target="_blank">
                 See Site
